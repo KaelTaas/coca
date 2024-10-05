29805b3b786c1c8b48d032d09ee5b65cda7ff309
Edward <greygward.gmail.com>
2024-10-05 17:46:43 +0300

    created contact: hero section

---
+++ src/js/contact.js
@@ -0,0 +1,7 @@
+import '../assets/scss/contact.scss';
+
+// components
+import './components/theme';
+import { useBurger } from './components/burger.js';
+
+useBurger();
--- src/js/pricing.js
+++ src/js/pricing.js
@@ -4,7 +4,6 @@ import '../assets/scss/pricing.scss';
 import './components/theme';
 import { useBurger } from './components/burger.js';
 import { usePriceSwitcher } from './components/pricing/priceSwitcher.js';
-// import './components/pricing/priceSwitcher.js';
 
 useBurger();
 usePriceSwitcher();
