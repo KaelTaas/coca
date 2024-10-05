4fb23e6ff92a8381b894e20697108358e0c546d5
Edward <greygward.gmail.com>
2024-10-05 22:28:00 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: './src',
+  base: '/coca/',
   server: {
     port: 3000,
     open: true,
   },
+  publicDir: '.src/assets',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
