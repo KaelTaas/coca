01359d31546c48c0d79da4ae2fe159a66dc84bca
Edward <greygward.gmail.com>
2024-10-05 14:14:05 +0300

    created pricing: hero section

---
+++ src/js/pricing.js
@@ -0,0 +1,10 @@
+import '../assets/scss/pricing.scss';
+
+// components
+import './components/theme';
+import { useBurger } from './components/burger.js';
+import { usePriceSwitcher } from './components/pricing/priceSwitcher.js';
+// import './components/pricing/priceSwitcher.js';
+
+useBurger();
+usePriceSwitcher();
